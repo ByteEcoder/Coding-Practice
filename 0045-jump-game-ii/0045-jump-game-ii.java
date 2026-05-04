2996fda910140a2399d27d685c69a272073109e6
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-05-05 02:10:53 +0530

    Time: 3 ms (17.5%), Space: 47.5 MB (20.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int jump(int[] nums) 
-    {
-        int far=0;
-        int end=0;
+    public int jump(int[] nums) {
+        int n=nums.length;
+        int l=0;
+        int r=0;
         int jumps=0;
-        
-        for(int i=0;i<nums.length-1;i++)
-        {
-            far=Math.max(far,i+nums[i]);
-
-            if(i==end)
+        while(r<n-1)
+        { 
+            int far=0;
+            for(int i=l;i<=r;i++)
             {
-                end=far;
-                jumps++;
+                far=Math.max(far,i+nums[i]);
             }
+            l++;
+            r=far;
+            jumps++;
         }
-
         return jumps;
     }
 }
-
-//
