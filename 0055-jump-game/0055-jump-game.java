0797a35bf6b2a4932349877f4ed032da39919235
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-05-04 09:11:31 +0530

    Time: 2 ms (87.05%), Space: 47.9 MB (41.23%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-      int max=nums[0];
-        int i=0;
-      while(i<nums.length&&i<=max)
-      {
-        if(i+nums[i]>max)
-            max=i+nums[i];
+        int max=nums[0];
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i<=max)
+            {
+                if(i+nums[i]>max)
+                    max=i+nums[i];
+            }
+        }
+
+        if(max>=nums.length-1)
+            return true;
         
-        
-        i++;
-
-
-      }
-
-      if(max>=nums.length-1)
-        return true;
-
-
-      return false;
-
+        return false;
     }
 }
