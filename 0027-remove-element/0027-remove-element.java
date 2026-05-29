874550763156177bd9a88d4dafb5747f1babc6e6
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-05-30 01:26:15 +0530

    Time: 0 ms (100%), Space: 43.3 MB (88.59%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int index = 0; // Pointer to track the new length of the array
+        int index = 0; 
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[index] = nums[i]; // Move valid element to index position
-                index++; // Increment index
+                nums[index] = nums[i]; 
+                index++; 
             }
         }
         
-        return index; // Return new length after removing elements
+        return index; 
     }
 }
