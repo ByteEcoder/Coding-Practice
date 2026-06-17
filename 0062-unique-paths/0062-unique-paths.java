99999c95b2b19c73d22cbb52e0ff94d91184662b
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-06-17 21:59:57 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -10,14 +10,13 @@ class Solution {
 
     public int helper(int i, int j, int m, int n, int[][] dp) {
         if (i == m - 1 && j == n - 1)
-            return 1;
+            return 1; 
+        if(i+1>m||j+1>n)
+            return 0;
         if (dp[i][j] != -1)
             return dp[i][j];
-        int d = 0, r = 0;
-        if (i + 1 < m)
-            d = helper(i + 1, j, m, n, dp);
-        if (j + 1 < n)
-            r = helper(i, j + 1, m, n, dp);
+        int d = helper(i + 1, j, m, n, dp);
+        int r = helper(i, j + 1, m, n, dp);
 
         return dp[i][j] = d + r;
     }
