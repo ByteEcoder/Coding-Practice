6df34f7aef3f4c56d7de5648e00672f0f3d7e847
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-09-11 22:48:17 +0530

    Time: 5 ms (82.8%), Space: 46.2 MB (58.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,25 +1,25 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        int n=digits.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++)
-        {
-           
-            if(digits[i]==0)
-                continue;
-
-            for(int j=0;j<n;j++)
-            {   
-                if(i==j)
-                    continue;
+        HashSet<Integer> map = new HashSet<>();
+        int n = digits.length;
         
-                for(int k=0;k<n;k++)
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                for (int k = 0; k < n; k++) 
                 {
-                    int curr=100*digits[i]+10*digits[j]+digits[k];
-                    if(i==k||j==k||digits[k]%2!=0||map.containsKey(curr))
-                        continue;
-                    map.put(curr,1);
-                    
+                    if (i != j && i != k && j != k) 
+                    {
+                        if (digits[i] != 0) 
+                        {
+                            if (digits[k] % 2 == 0) 
+                            {
+                                int number = digits[i] * 100 + digits[j] * 10 + digits[k];
+                                map.add(number);
+                            }
+                        }
+                    }
                 }
             }
         }
